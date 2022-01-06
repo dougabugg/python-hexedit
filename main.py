a556b4fac16be1029d2431560656da9b365c9894
lthon09 <>
2022-01-06 21:54:45 +0700

    remove o file selected, on exit messagebox

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
-from tkinter import Tk, Menu, filedialog
+from tkinter import Tk, Menu, filedialog, messagebox
 from tkinter.scrolledtext import ScrolledText
 
+edited = False
+_open = None
+
 root = Tk()
+
 root.title("HexEdit")
 
 root.grid_columnconfigure(0, weight=1)
@@ -14,16 +18,24 @@ file_dialog = filedialog.Open(master=root, title="Open a File", initialdir=os.ge
 
 text_out = ScrolledText(master=root)
 text_out.grid(sticky="news")
+
 text_out["state"] = "disabled"
 
-def _open():
+def __open():
     text_out["state"] = "normal"
+
     text_out.delete("1.0", "end")
+
     _hex = open_file()
-    text_out.insert("1.0", _hex)
+
+    if _hex:
+        text_out.insert("1.0", _hex)
+
     text_out["state"] = "disabled"
 
 def open_file():
+    global _open
+
     path = file_dialog.show()
 
     if path:
@@ -33,21 +45,35 @@ def open_file():
         _hex = ""
 
         for byte in content:
-            _next_char = ('%x' % byte)
+            _next_char = ("%x" % byte)
+
             if len(_next_char) == 1:
                 _next_char = "0" + _next_char
+
             _hex += _next_char + " "
+
+        _open = path
 
         return _hex
 
-    return "No File Selected."
-
 def save():
-    pass
+    if _open:
+        pass # save
 
-menu.add_command(label="Open", command=_open)
+menu.add_command(label="Open", command=__open)
 menu.add_command(label="Save", command=save)
 
 root.config(menu=menu)
+
+def _exit():
+    if edited:
+        warning = messagebox.askyesno("HexEdit", "You have unsaved changes. Are you sure you want to exit?", icon="warning")
+
+        if warning:
+            exit()
+    else:
+        exit()
+
+root.protocol("WM_DELETE_WINDOW", _exit)
 
 root.mainloop()
